3ddfc14c8af42a44c9acee574468abb41745e2cb
Nicole Daniels <nzoner12@yahoo.com>
2018-03-16 00:54:54 -0400

    Add header and fix styling

--- app/screens/control.js
+++ app/screens/control.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements'
 
 export default class Control extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>This is the Control Page</Text>
+      <View style={styles.controlContainer}>
+        <Header
+          centerComponent={{text: 'On/Off', style: {color: '#fff'}}}
+        />
+        <View style={styles.textContainer}>
+          <Text>This is the Control Page</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  controlContainer: {
+    flex: 1,
+  },
+  textContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
--- app/screens/feed.js
+++ app/screens/feed.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from 'react-native-elements';
 
 export default class Feed extends React.Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>This is the Feed Page</Text>
+      <View style={styles.feedContainer}>
+        <Header
+          centerComponent={{text: 'Feed', style: {color: '#fff'}}} 
+        />
+        <View style={styles.textContainer}>
+          <Text>This is the Feed Page</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  feedContainer: {
+    flex: 1,
+  },
+  textContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
